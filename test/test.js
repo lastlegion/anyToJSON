2b514f55df91ece43596fdec75e2e12ac962fbc4
Ganesh Iyer <lastlegion@gmail.com>
2015-05-23 22:48:33 +0530

    added tests

--- test/test.js
+++ test/test.js
@@ -6,11 +6,21 @@ describe("anyToJSON", function(){
     describe("csv to json", function(){
         it("should convert csv to json", function(done){
             anyToJSON.csv({path: "100.csv"}, function(){
-                console.log(anyToJSON.data);
                 var output = [{"a":"4","b":"9","c":"2","d":"3"}]
                 assert.equal(JSON.stringify(anyToJSON.data), JSON.stringify(output));
                 done()
             });
+        });
+        it("should not break when there are commas in strings", function(done){
+            anyToJSON.csv({path: "test.csv"}, function(){
+                var output = [{"Id":"1","UserName":"Sam, Smith"},
+                                {"Id":"2","UserName":"Fred Frankly"},
+                                {"Id":"1","UserName":"Zachary Zupers"}];
+                assert.equal(JSON.stringify(anyToJSON.data), JSON.stringify(output));
+                done();
+            })
         })
-    })
-})
+    });
+
+
+});
