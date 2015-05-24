b3387684da380ba28b35b7b28b9626abd988909f
Ganesh Iyer <lastlegion@gmail.com>
2015-05-24 16:54:51 +0530

    added csv tests

--- test/test.js
+++ test/test.js
@@ -5,14 +5,14 @@ var assert = require("assert");
 describe("anyToJSON", function(){
     describe("csv to json", function(){
         it("should convert csv to json", function(done){
-            anyToJSON.csv({path: "100.csv"}, function(){
+            anyToJSON.csv({path: "test/100.csv"}, function(){
                 var output = [{"a":"4","b":"9","c":"2","d":"3"}]
                 assert.equal(JSON.stringify(anyToJSON.data), JSON.stringify(output));
                 done()
             });
         });
         it("should not break when there are commas in strings", function(done){
-            anyToJSON.csv({path: "test.csv"}, function(){
+            anyToJSON.csv({path: "test/test.csv"}, function(){
                 var output = [{"Id":"1","UserName":"Sam, Smith"},
                                 {"Id":"2","UserName":"Fred Frankly"},
                                 {"Id":"1","UserName":"Zachary Zupers"}];
