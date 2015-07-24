9cf9ebc8fb7e9ba8924307761fe6731a94f0acd3
Ganesh Iyer <lastlegion@gmail.com>
2015-07-24 17:59:29 +0530

    removed odbc test

--- test/test.js
+++ test/test.js
@@ -10,7 +10,7 @@ describe("anyToJSON", function(){
                 var output = [{"a":"4","b":"9","c":"2","d":"3"}];
                 var anyToJSONdata = data;
                 assert.equal(JSON.stringify(anyToJSONdata), JSON.stringify(output));
-                done()
+                done();
             });
         });
         it("should not break when there are commas in strings", function(done){
@@ -21,8 +21,8 @@ describe("anyToJSON", function(){
                 anyToJSONdata = data;
                 assert.equal(JSON.stringify(anyToJSONdata), JSON.stringify(output));
                 done();
-            })
-        })
+            });
+        });
     });
     describe("loading json from a file", function(){
         it("should load json from a file", function(done){
@@ -31,9 +31,9 @@ describe("anyToJSON", function(){
                 var output = [{"a":"4","b":"9","c":"2","d":"3"}];
                 anyToJSONdata = data;
                 assert.equal(JSON.stringify(anyToJSONdata), JSON.stringify(output));
-                done()
+                done();
             });  
-        })
+        });
     });
     describe("loading json from rest", function(){
         it("should load json from rest", function(done){
@@ -46,9 +46,9 @@ describe("anyToJSON", function(){
                         var anyToJSONdata = data;
                         assert.equal(JSON.stringify(anyToJSONdata), JSON.stringify(output));
                         done();
-                })
+                });
             });
-        })
+        });
     });
     describe("loading csv from rest", function(){
         it("should load csv from rest", function(done){
@@ -58,19 +58,21 @@ describe("anyToJSON", function(){
                     port: 3000,
                     path: "/csv"}, function(data){
                         anyToJSONdata = data;
-                        var output = [{"a":"4","b":"9","c":"2","d":"3"}]
+                        var output = [{"a":"4","b":"9","c":"2","d":"3"}];
                         assert.equal(JSON.stringify(anyToJSONdata), JSON.stringify(output));
                         done();
                 
-                })
-            })
-        })
+                });
+            });
+        });
     });
+    /*
     describe("loading odbc", function(){
         it("should try to connect", function(done){
             anyToJSON.odbc({cn: "dsn=mysql;server=localhost;user=root;database=test;port=3306;password=123456;command timeout=30000;", table: "titanicClean"}, function(){
                 done();
-            })
-        })
-    })
+            });
+        });
+    });
+    */
 });
